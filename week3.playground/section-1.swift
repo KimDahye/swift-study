386bb33535a3fd4640a5cf9d65ce98e10a51512a
Dahye Kim <dahye.kim@nhnnext.org>
2015-07-14 20:42:31 +0900

    week3 enum example

--- week3.playground/section-1.swift
+++ week3.playground/section-1.swift
@@ -13,4 +13,51 @@ enum JSONNode {
     case ObjectNode([String:JSONNode])
 }
 
-let x : JSONNode = .ArrayNode 
+let x : JSONNode = .ArrayNode(
+    [.NumberNode(10.0),
+    .StringNode("hello"),
+    .BoolNode(false)
+    ]
+)
+
+let y : JSONNode = .ObjectNode(
+    [
+        "one" : .NullNode,
+        "two" : x,
+        "three": .StringNode("hi")
+    ]
+)
+
+func jsonStringify(json: JSONNode) -> String {
+    switch json {
+    case .NullNode :
+        return "null"
+    case .StringNode(let str) :
+        return "\"" + str + "\""
+    case .NumberNode(let num) :
+        return toString(num)
+    case .BoolNode(let bool) :
+        return toString(bool)
+    case .ArrayNode(let jsonArr) :
+        var arr:[String] = []
+        for node in jsonArr {
+            arr.append(jsonStringify(node))
+        }
+        return "[" + ", ".join(arr) + "]"
+    case .ObjectNode(let dic) :
+        str = "["
+        for key in dic.keys {
+            str += "(\(key) :"
+            str += jsonStringify(dic[key]!)
+            str += ") "
+        }
+        str += "]"
+        return str
+    default :
+        return "default"
+    }
+}
+
+jsonStringify(x)
+print(jsonStringify(y))
+
